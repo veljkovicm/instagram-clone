9316879896034b796b947f5949ba9a9ed3ee6a88
Milan <comiv92@gmail.com>
2020-10-12 16:20:26 +0200

    Update user reducer

--- instagram-frontend/src/core/user/reducer.js
+++ instagram-frontend/src/core/user/reducer.js
@@ -2,11 +2,13 @@ import { actionTypes } from './actions.js';
 
 const initialState = {
   currentUser: null,
+  token: localStorage.getItem('token'),
 };
 
 export default (state = initialState, action) => {
   switch (action.type) {
     case actionTypes.SET_USER:
+      action.data.token && localStorage.setItem('token', action.data.token)
       return {
         ...state,
         currentUser: {
@@ -17,8 +19,10 @@ export default (state = initialState, action) => {
       };
 
     case actionTypes.CLEAR_USER:
+      localStorage.removeItem('token');
       return {
         ...state,
+        token: null,
         currentUser: null,
       };
 
