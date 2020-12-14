0a38640074578fb4a283e0247a04191ddba697f1
Milan <comiv92@gmail.com>
2020-12-14 16:25:45 +0100

    Feat: add settings page styling

--- instagram-frontend/src/scenes/Settings/SettingsPage.jsx
+++ instagram-frontend/src/scenes/Settings/SettingsPage.jsx
@@ -1,8 +1,8 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import Header from 'templates/components/Header';
 
-import './settingsPage.css'
+import './settingsPage.scss'
 
 const Settings = (props) => {
   const {
@@ -19,10 +19,15 @@ const Settings = (props) => {
   const [ bio, setBio ] = useState(props.user.bio || '');
   const [ phoneNumber, setPhoneNumber ] = useState(props.user.phoneNumber || '');
   const [ gender, setGender ] = useState(props.user.gender || '');
+  const [ avatar, setAvatar ] = useState(`http://localhost:5000/uploads/${props.user.avatar}` ||  'http://localhost:5000/uploads/no-img.png'); // refactor, urls should not be visible
+  const [ settingsChanged, setSettingsChanged ] = useState(false);
 
 
   const handleChange = (setValue) => (e) => {
     setValue(e.target.value);
+    if(!settingsChanged) {
+      setSettingsChanged(true);
+    }
   }
 
   const handleAvatarClick = () => {
@@ -33,7 +38,9 @@ const Settings = (props) => {
     e.preventDefault();
     const formData = new FormData();
     formData.append('file', e.target.files[0]);
-    uploadAvatar({ formData });
+    uploadAvatar({ formData }).then((res) => {
+      setAvatar(res.newAvatar);
+    });
   }
 
   const handleSubmit = () => {
@@ -49,65 +56,173 @@ const Settings = (props) => {
     updateSettings(userData);
   }
 
-  const avatarSrc = props.user.avatar ? `http://localhost:5000/uploads/${props.user.avatar}` : 'http://localhost:5000/uploads/no-img.png';
-
   return (
     <>
-    <Header />
-    <div className="settings">
-      <div className="settings_profile">
-        <form className="settings__profile-avatar-form" onSubmit={handleAvatarSubmit}>
-          <div className="settings__profile-left">
-            <div className="settings__profile-left__avatar" onClick={handleAvatarClick} style={{ backgroundImage: `url(${avatarSrc})`}}>
-              <div className="settings__profile_left__avatar-hover"></div>
-            </div>
-            <input type="file" hidden name="avatar1" onChange={handleAvatarSubmit} ref={(input) => { avatarInput = input; }} />
+      <Header />
+      <div className="page-content">
+        <div className="settings__page">
+          <div className="settings__tabs">
+            <a href="#" className="settings__tabs__link active">Edit profile</a>
+            <a href="#" className="settings__tabs__link forbidden">Change Password</a>
+            <a href="#" className="settings__tabs__link forbidden">Apps and Websites</a>
+            <a href="#" className="settings__tabs__link forbidden">Email and SMS</a>
+            <a href="#" className="settings__tabs__link forbidden">Push Notifications</a>
+            <a href="#" className="settings__tabs__link forbidden">Manage Contacts</a>
+            <a href="#" className="settings__tabs__link forbidden">Privacy and Security</a>
+            <a href="#" className="settings__tabs__link forbidden">Login Activity</a>
+            <a href="#" className="settings__tabs__link forbidden">Emails from Instagram</a>
           </div>
-          <div className="settings__profile-right">
-            <div className="settings__profile-right__username">
-              {username}
-            </div>
-            <div className="settings__profile-right__avatar-link">
-              <span onClick={handleAvatarClick}>Change Profile Photo</span>
+          <div className="settings__profile">
+              <div className="settings__profile__section">
+                <div className="settings__profile__section__label">
+                  <div
+                    className="settings__profile__avatar"
+                    onClick={handleAvatarClick}
+                    style={{ backgroundImage: `url(${avatar})`}}
+                  />
+                </div>
+                <div className="settings__profile__username-wrapper">
+                  <h4 className="settings__profile__username">{username}</h4>
+                  <span className="settings__profile__avatar-link" onClick={handleAvatarClick}>Change Profile Photo</span>
+                </div>
+                <form className="settings__profile__avatar-form" onSubmit={handleAvatarSubmit}>
+                  <input
+                    type="file"
+                    hidden
+                    name="avatar"
+                    onChange={handleAvatarSubmit}
+                    ref={(input) => { avatarInput = input }}
+                  />
+                </form>
+              </div>
+            <div className="settings__profile__form__wrapper">
+              <form className="settings__profile__form" onSubmit={handleSubmit}>
+                <div className="settings__profile__section">
+                  <div className="settings__profile__section__label">
+                    <label htmlFor="name">Name</label>
+                  </div>
+                  <div className="settings__profile__section__input-wrapper">
+                    <input
+                      className="settings__profile__section__input"
+                      type="text"
+                      name="name"
+                      onChange={handleChange(setName)}
+                      value={name}
+                    />
+                    <div className="settings__profile__section__description">
+                      <span>Help people discover your account by using the name you're known by: either your full name, nickname, or business name.</span>
+                      <span>You can only change your name twice within 14 days.</span>
+                    </div>
+                  </div>
+                </div>
+                <div className="settings__profile__section">
+                  <div className="settings__profile__section__label">
+                    <label htmlFor="username">Username</label>
+                  </div>
+                  <div className="settings__profile__section__input-wrapper">
+                    <input
+                      className="settings__profile__section__input"
+                      type="text"
+                      name="username"
+                      onChange={handleChange(setUsername)}
+                      value={username}
+                      placeholder="Username"
+                    />
+                    <div className="settings__profile__section__description">
+                      <span>
+                        In most cases, you'll be able to change your username back to kaplareviccobe for another 14 days.
+                        <a href="#" className="forbidden">Learn more</a>
+                      </span>
+                    </div>
+                  </div>
+                </div>
+                <div className="settings__profile__section">
+                  <div className="settings__profile__section__label">
+                    <label htmlFor="website">Website</label>
+                  </div>
+                  <input
+                    className="settings__profile__section__input"
+                    type="text"
+                    name="website"
+                    onChange={handleChange(setWebsite)}
+                    value={website}
+                    placeholder="Website"
+                  />
+                </div>
+                <div className="settings__profile__section">
+                  <div className="settings__profile__section__label">
+                    <label htmlFor="bio">Bio</label>
+                  </div>
+                  <div className="settings__profile__section__input-wrapper">
+                    <textarea
+                      className="settings_profile__section__textarea"
+                      name="bio"
+                      cols="30"
+                      rows="3"
+                      onChange={handleChange(setBio)}
+                      value={bio}
+                    />
+                    <div className="settings__profile__section__description">
+                      <span>
+                        <b>Personal information</b>
+                        Provide your personal information, even if the account is used for a business, a pet or something else. This won't be a part of your public profile.
+                      </span>
+                    </div>
+                  </div>
+                </div>
+                <div className="settings__profile__section">
+                  <div className="settings__profile__section__label">
+                    <label htmlFor="email">Email</label>
+                  </div>
+                  <div className="settings__profile__section__input-wrapper">
+                    <input
+                      className="settings__profile__section__input"
+                      type="text"
+                      name="email"
+                      onChange={handleChange(setEmail)}
+                      value={email}
+                      placeholder="Email"
+                    />
+                  </div>
+                </div>
+                <div className="settings__profile__section">
+                  <div className="settings__profile__section__label">
+                    <label htmlFor="phone-number">Phone Number</label>
+                  </div>
+                  <input
+                    className="settings__profile__section__input"
+                    type="text"
+                    name="phone-number"
+                    onChange={handleChange(setPhoneNumber)}
+                    value={phoneNumber}
+                    placeholder="Phone Number"
+                  />
+                </div>
+                <div className="settings__profile__section">
+                  <div className="settings__profile__section__label">
+                    <label htmlFor="gender">Gender</label>
+                  </div>
+                  <input
+                    className="settings__profile__section__input"
+                    type="text"
+                    name="gender"
+                    onChange={handleChange(setGender)}
+                    value={gender}
+                    placeholder="Gender"
+                  />
+                </div>
+              </form>
+              <button
+                className="settings__profile__form__button"
+                onClick={handleSubmit}
+                disabled={!settingsChanged}
+              >
+                Submit
+              </button>
             </div>
           </div>
-        </form>
+        </div>
       </div>
-      <div className="settings__profile-form__wrapper">
-        <form className="settings__profile-form" onSubmit={handleSubmit}>
-          <div className="settings__profile-form__name">
-            <label htmlFor="name">Name</label>
-            <input type="text" name="name" onChange={handleChange(setName)} value={name} />
-          </div>
-          <div className="settings__profile-form__username">
-            <label htmlFor="username">Username</label>
-            <input type="text" name="username" onChange={handleChange(setUsername)} value={username} />
-          </div>
-          <div className="settings__profile-form__website">
-            <label htmlFor="website">Website</label>
-            <input type="text" name="website" onChange={handleChange(setWebsite)} value={website} />
-          </div>
-          <div className="settings__profile-form__bio">
-            <label htmlFor="bio">Bio</label>
-            <input type="text" name="bio" onChange={handleChange(setBio)} value={bio} />
-          </div>
-          <div className="settings__profile-form__email">
-            <label htmlFor="email">Email</label>
-            <input type="text" name="email" onChange={handleChange(setEmail)} value={email} />
-          </div>
-          <div className="settings__profile-form__phone-number">
-            <label htmlFor="phone-number">Phone Number</label>
-            <input type="text" name="phone-number" onChange={handleChange(setPhoneNumber)} value={phoneNumber} />
-          </div>
-          <div className="settings__profile-form__gender">
-            <label htmlFor="gender">Gender</label>
-            <input type="text" name="gender" onChange={handleChange(setGender)} value={gender}/>
-          </div>
-        </form>
-      </div>
-      <button onClick={handleSubmit}>Submit</button>
-    
-    </div>
     </>
   )
 }
